fe95260801e7f416edc5cd5b9838f2223bd1bbe9
takuya.kikuchi <k.takuya423@gmail.com>
2021-03-08 04:42:59 +0900

    update:issue対応

--- src/scripts/modules/Anchorlink.js
+++ src/scripts/modules/Anchorlink.js
@@ -2,32 +2,32 @@ export default class anchorlink {
   constructor(elem, opts) {
     this.elem = elem
     this.pageName = opts.pageName
-
     this.url = location.href
-    this.origin = location.origin
-    this.page = this.url.replace(this.origin, '')
-    this.parameters = this.page.replace(`/${this.pageName}/`, '')
-    this.target = this.parameters.replace('?target=', '')
 
-    this.headerHeight = document.getElementsByClassName('c-header')[0].clientHeight
+    if (this.url.indexOf('?target=') !== -1) {
+      this.origin = location.origin
+      this.page = this.url.replace(this.origin, '')
+      this.parameters = this.page.replace(`/${this.pageName}/`, '')
+      this.target = this.parameters.replace('?target=', '')
 
-    this.toY
-    this.currentY = window.pageYOffset
-    this.divisor = 10
-    this.range = (this.divisor / 2) + 1;
-    this.targetElem = document.getElementById(this.target)
-    this.targetRect = this.targetElem.getBoundingClientRect()
-    this.targetY = this.targetRect.top + this.currentY - this.headerHeight
+      this.headerHeight = document.getElementsByClassName('c-header')[0].clientHeight
 
-    this.init()
+      this.toY
+      this.currentY = window.pageYOffset
+      this.divisor = 10
+      this.range = (this.divisor / 2) + 1;
+      this.targetElem = document.getElementById(this.target)
+      this.targetRect = this.targetElem.getBoundingClientRect()
+      this.targetY = this.targetRect.top + this.currentY - this.headerHeight
+
+      this.init()
+    }
   }
   init() {
     this.getUrlParameters()
   }
   getUrlParameters() {
-    if (this.url.indexOf('?target=') !== -1) {
       this.smoothScroll()
-    }
   }
   smoothScroll() {
     this.toY = this.currentY + Math.round((this.targetY - this.currentY) / this.divisor)
